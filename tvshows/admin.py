244839837424c535ffb54ded81b2e8fd6692e830
OliviaJ <liqhta@gmail.com>
2021-02-28 13:44:54 +0100

    New models have been created for TvShow, Season, Episode and registered in the django admin. New empty index view for test.

--- tvshows/admin.py
+++ tvshows/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import TvShow, Season, Episode
 
-# Register your models here.
+admin.site.register(TvShow)
+admin.site.register(Season)
+admin.site.register(Episode)
--- tvshows/models.py
+++ tvshows/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class TvShow(models.Model):
+    title = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    poster = models.CharField(max_length=1000)
+    description = models.CharField(max_length=2500)
+    productionCountry = models.CharField(max_length=40)
+
+    def __str__(self):
+        return "[" + str(self.id) + "] " + self.title + " (" + self.productionCountry + ") - " + self.genre + "-" + self.description[0:100]
+
+
+class Season(models.Model):
+    tvShow = models.ForeignKey(TvShow, on_delete=models.CASCADE)
+    number = models.SmallIntegerField()
+
+    def __str__(self):
+        return self.tvShow.title + " - season " + str(self.number)
+
+
+class Episode(models.Model):
+    season = models.ForeignKey(Season, on_delete=models.CASCADE)
+    number = models.SmallIntegerField()
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=2500)
+    duration = models.DurationField()
+    releaseDate = models.DateField()
+
+    def __str__(self):        
+        return self.season.tvShow.title + " [S" + str(self.season.number) + "E" + str(self.number) + "] [duration: " + str(self.duration) + "] released: " + str(self.releaseDate)
